410da2c61ccc31b4aebd9f2cc6f983cdf1704f67
saibotfn <tobiasfndk@gmail.com>
2025-09-22 23:06:05 +0200

    I made the enemies indvidually damageable

--- Assets/Scripts/enemyBehavior.cs
+++ Assets/Scripts/enemyBehavior.cs
@@ -14,20 +14,10 @@ public class enemyBehavior : MonoBehaviour
         for(int i = 0; i<enemy.Length; i++)
         {
 
-            if(enemy[i] == null) return;
+            if(enemy[i] == null) continue;
             enemy[i].GetComponent<NavMeshAgent>().SetDestination(player.position);
-            Ray ray = new Ray(player.position, player.forward);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 100f))
-            {
-                if(hit.collider.gameObject.tag == "Enemy")
-                {
-                    enemyHealth[i] -= Time.deltaTime;
-                    enemy[i].GetComponent<NavMeshAgent>().SetDestination(enemy[i].transform.position);
 
-                }
-                
-            }
+
             if(enemyHealth[i] <= 1f)
             {
                 enemy[i].GetComponent<ParticleSystem>().Play();
@@ -38,6 +28,26 @@ public class enemyBehavior : MonoBehaviour
                 GameObject.Destroy(enemy[i]);
             }
         }
-        
+
+        Ray ray = new Ray(player.position, player.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            if (hit.collider.gameObject.tag == "Enemy")
+            {
+                for (int i = 0; i < enemy.Length; i++)
+                {
+                    if (enemy[i] == null) continue;
+                    if (hit.collider.gameObject == enemy[i])
+                    {
+                        enemyHealth[i] -= Time.deltaTime;
+                        enemy[i].GetComponent<NavMeshAgent>().SetDestination(enemy[i].transform.position);
+                        break;
+                    }
+                }
+            }
+
+        }
+
     }
 }
