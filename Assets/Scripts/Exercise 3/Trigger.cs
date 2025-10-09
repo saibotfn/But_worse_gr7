3d2c05913b2e4ee4c0b60b929766fd66ead63aec
AReallyFatGoat <zachasaks@gmail.com>
2025-10-09 12:25:50 +0200

    Moving Platform

--- Assets/Scripts/Exercise 3/Trigger.cs
+++ Assets/Scripts/Exercise 3/Trigger.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Trigger : MonoBehaviour
 
 {
     private bool active = false;
-    [SerializeField] private Vector3[] path;
+    [SerializeField] private List <Vector3> path = new List <Vector3>() ;
+    private int currentTarget = 0;
+    [SerializeField] private int speed;
 
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
         active = true;
     }
@@ -15,7 +18,16 @@ public class Trigger : MonoBehaviour
     void Update()
     {
         if (!active) { return; }
-
-
+        if ((path[currentTarget] - transform.position).magnitude <= 0.1f)
+        {
+            transform.position = path[currentTarget];
+            currentTarget++;
+        }
+        else
+        {
+            Debug.Log(path.Count);
+            Vector3 tempVector = (path[currentTarget] - transform.position).normalized;
+            transform.position += tempVector * speed * Time.deltaTime;
+        }
     }
 }
